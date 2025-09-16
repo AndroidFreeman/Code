db61dd1a8b5779b7638652dd803397c0243666f9
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-09-16 11:50:44 +0000

    Tue, Sep 16, 2025, 7:50 PM +08:00

--- test2.c
+++ test2.c
@@ -35,6 +35,14 @@ int main(){
     if(income<=750){
         printf("%f",income*0.01);
     }else if(income<=2250){
-        printf("%f",7.50+0.02*(income-750))
+        printf("%f",7.50+0.02*(income-750));
+    }else if(income<=3750){
+        printf("%f",37.5+0.03*(income-2250));
+    }else if(income<=5250){
+        printf("%f",82.5+0.04*(income-3750));
+    }else if(income<=7000){
+        printf("%f",142.5+0.05*(income-5250));
+    }else{
+        printf("%f",230+0.06*(income-7000));
     }
 }
