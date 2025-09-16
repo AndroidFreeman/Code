6c7db1eb1e39554ee5b47c41e69dce2127ad5cd8
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-09-16 12:09:45 +0000

    Tue, Sep 16, 2025, 8:09 PM +08:00

--- test2.c
+++ test2.c
@@ -47,9 +47,11 @@ int main(){
     // }
 
     int n1,n2,n3,n4;
-    int temp1,temp2,temp3,temp4;
+    int max,min;
     scanf("%d %d %d %d",&n1,&n2,&n3,&n4);
-    if("n1>=n2"){
-        
+    max=n1;
+    min=n1;
+    if("n2>max"){
+
     }
 }
