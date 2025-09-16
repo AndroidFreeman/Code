f907beffa85faff7cf3d68b11a3d16dfa99d7f3d
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-09-16 11:44:50 +0000

    Tue, Sep 16, 2025, 7:44 PM +08:00

--- test2.c
+++ test2.c
@@ -12,5 +12,29 @@ int main(){
     // }else{
     //     printf("Equicalent 12-hour time: %d:%d %s \n",hour,minute,time);
     // }
-    
+    // printf("Enter your wind:");
+    // int ver;
+    // scanf("%d",&ver);
+    // if (ver<1){
+    //     printf("Calm");
+    // }else if(ver<4){
+    //     printf("Light air");
+    // }else if(ver<28){
+    //     printf("Breeze");
+    // }else if(ver<48){
+    //     printf("Gale");
+    // }else if(ver<64){
+    //     printf("Storm");
+    // }else{
+    //     printf("Hurricane");
+    // }
+
+    float income;
+    printf("Enter your income:");
+    scanf("%f",&income);
+    if(income<=750){
+        printf("%f",income*0.01);
+    }else if(income<=2250){
+        printf("%f",7.50+0.02*(income-750))
+    }
 }
