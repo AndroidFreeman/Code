eb313d186939868879d8a001e711c0252f356929
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-09-16 10:42:45 +0000

    Tue, Sep 16, 2025, 6:42 PM +08:00

--- test2.c
+++ test2.c
@@ -2,7 +2,12 @@
 int main(){
     printf("Enter a 24-hour time:");
     int hour,minute;
+
     scanf("%d:%d",&hour,&minute);
-    
+    if(hour>12){
+        hour=hour-12;
+        char time[]="PM";
+    }
+    printf("Equicalent 12-hour time: %d:%d %s",hour,minute,time);
 
 }
