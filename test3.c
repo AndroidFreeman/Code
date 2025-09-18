0dc2240d2ff0cddc17ea0740a989c24b0e3608a7
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-09-18 23:54:10 +0000

    Fri, Sep 19, 2025, 7:54 AM +08:00

--- test3.c
+++ test3.c
@@ -35,18 +35,41 @@ int main(){
     // deposit=capital+capital*year*(rate/100);
     // printf("Your deposit is: %f",deposit);
 
-    float r1,h1,r2,h2;
-    float v1,s1,v2,s2;
-    printf("Circular cone's r h:");
-    scanf("%f%f",&r1,&h1);
-    printf("Cylinder's r h:");
-    scanf("%f%f",&r2,&h2);
-    v1 = PI*r1*r1*h1/3;
-    s1 = PI*r1*r1+PI*r1*(sqrt(r1*r1+h1*h1));
-    v2 = PI*r2*r2*h2;
-    s2 = 2*PI*r2*r2+2*PI*r2*h2;
-    printf("Circular cone v:%f s:%f",v1,s1);
-    printf("Cylinder v:%f s:%f",v2,s2);
+    // float r1,h1,r2,h2;
+    // float v1,s1,v2,s2;
+    // printf("Circular cone's r h:");
+    // scanf("%f%f",&r1,&h1);
+    // printf("Cylinder's r h:");
+    // scanf("%f%f",&r2,&h2);
+    // v1 = PI*r1*r1*h1/3;
+    // s1 = PI*r1*r1+PI*r1*(sqrt(r1*r1+h1*h1));
+    // v2 = PI*r2*r2*h2;
+    // s2 = 2*PI*r2*r2+2*PI*r2*h2;
+    // printf("Circular cone v:%f s:%f",v1,s1);
+    // printf("Cylinder v:%f s:%f",v2,s2);
 
+    // char number;
+    // printf("Enter a char:");
+    // scanf("%c",&number);
+    // printf("%d",number);
 
+    // char input_char;
+    // printf("Enter a char:");
+    // scanf(" %c", &input_char);
+    // if (input_char >= 'a' && input_char <= 'z') {
+    //     char uppercase_char = input_char - 32;
+    //     printf("Char: %c\n", uppercase_char);
+    //     printf("10: %d\n", uppercase_char);
+    //     printf("16: %X\n", uppercase_char);
+    // } else {
+    //     printf("Wrong\n");
+    // }
+
+    // char c1,c2;
+    // int n1,n2;
+    // c1=getchar();
+    // c2=getchar();
+    // n1=c1-'0';
+    // n2=n1*10+(c2-'0');
+    // printf("%d\n",n2);
 }
---
+++ test4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    // int len=0;
+    // printf("Enter a message: ");
+    // while(getchar()!='\n'){
+    //     len++;
+    // }
+    // printf("Your length is %d\n",len);
+
+    // int eight=077;
+    // int sixteen1=0x77;
+    // int sixteen2=0XABC;
+    // printf("%d %d %d",eight,sixteen1,sixteen2);
+
+    printf("Enter phone number:");
+    int pNum;
+    
+}
+
