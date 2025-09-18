b09c4b7cffd1ceb447a080513a728654d4f2179c
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-09-18 00:31:38 +0000

    Thu, Sep 18, 2025, 8:31 AM +08:00

--- test3.c
+++ test3.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
 int main(){
-    
+    int sum=0;
+    int i;
+    printf("Enter a number:");
+    scanf("%d",&i);
+    do{
+        sum=sum+1;
+        i++;
+    }while(i<=10);
+    printf("sum=%d",sum);
 }
