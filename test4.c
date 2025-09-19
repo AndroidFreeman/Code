ac4581c3a91e59e1de247317a2bf510ba2494794
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-09-19 00:38:57 +0000

    Fri, Sep 19, 2025, 8:38 AM +08:00

--- test4.c
+++ test4.c
@@ -13,38 +13,40 @@ int main(){
     // int sixteen2=0XABC;
     // printf("%d %d %d",eight,sixteen1,sixteen2);
 
-    printf("Enter phone number:");
-    char ch;
-    while((ch=getchar())!='\n'){
-        switch(ch){
-            case 'A':case 'B':case 'C':
-                putchar('2');
-                break;
-            case 'D':case 'E':case 'F':
-                putchar('3');
-                break;
-            case 'G':case 'H':case 'I':
-                putchar('4');
-                break;
-            case 'J':case 'K':case 'L':
-                putchar('5');
-                break;
-            case 'M':case 'N':case 'O':
-                putchar('6');
-                break;
-            case 'P':case 'Q':case 'R':case 'S':
-                putchar('7');
-                break;
-            case 'T':case 'U':case 'V':
-                putchar('8');
-                break;
-            case 'W':case 'X':case 'Y':case 'Z':
-                putchar('9');
-                break;
-            default:
-                putchar(ch);
-                break;
-        }
-    }
+    // printf("Enter phone number:");
+    // char ch;
+    // while((ch=getchar())!='\n'){
+    //     switch(ch){
+    //         case 'A':case 'B':case 'C':
+    //             putchar('2');
+    //             break;
+    //         case 'D':case 'E':case 'F':
+    //             putchar('3');
+    //             break;
+    //         case 'G':case 'H':case 'I':
+    //             putchar('4');
+    //             break;
+    //         case 'J':case 'K':case 'L':
+    //             putchar('5');
+    //             break;
+    //         case 'M':case 'N':case 'O':
+    //             putchar('6');
+    //             break;
+    //         case 'P':case 'Q':case 'R':case 'S':
+    //             putchar('7');
+    //             break;
+    //         case 'T':case 'U':case 'V':
+    //             putchar('8');
+    //             break;
+    //         case 'W':case 'X':case 'Y':case 'Z':
+    //             putchar('9');
+    //             break;
+    //         default:
+    //             putchar(ch);
+    //             break;
+    //     }
+    // }
+
+    
 }
 
