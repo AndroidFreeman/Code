c0c9cb349911a4d8c44f227ed1fd1fa7a2308782
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-09-23 03:26:35 +0000

    Tue, Sep 23, 2025, 11:26 AM +08:00

--- learn1.cpp
+++ learn1.cpp
@@ -5,12 +5,15 @@ using namespace std;
 int main(){
     bool flag=true;
     bool flag=false;
-
+    string data;
     string str="Hello World!";
 
     int a = 10;
     cout<<"a="<<a<<endl;
     cout<<"Hello C++"<<endl;
+    cout<<"Enter your number:"<<endl;
+    cin>>data;
+    cout<<data;
 
     return 0;
 }
