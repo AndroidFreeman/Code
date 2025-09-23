bb7f378e4b055408ac6fc392e46f34a72befe23a
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-09-23 03:21:28 +0000

    Tue, Sep 23, 2025, 11:21 AM +08:00

--- learn1.cpp
+++ learn1.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
     string str="Hello World!";
-    
+
     int a = 10;
     cout<<"a="<<a<<endl;
     cout<<"Hello C++"<<endl;
