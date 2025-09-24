e0109cf97745330b1931dd5425d7432e6016e46c
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-09-24 10:44:15 +0000

    Wed, Sep 24, 2025, 6:44 PM +08:00

--- test8.c
+++ test8.c
@@ -1,4 +1,45 @@
 #include<stdio.h>
-int main(){
-    printf("Hello World!");
+#define N 100
+void max_min(int a[], int n, int *max, int *min);
+int main(void)
+{
+//  int b[N], i, big, small;
+//  printf("Enter %d numbers: ", N);
+//  for (i = 0; i < N; i++)
+//  scanf("%d", &b[i]);
+
+//  max_min(b, N, &big, &small);
+//  printf("Largest: %d\n", big);
+//  printf("Smallest: %d\n", small);
+//  return 0;
+// }
+// void max_min(int a[], int n, int *max, int *min)
+// {
+//  int i;
+//  *max = *min = a[0];
+//  for (i = 1; i < n; i++) {
+//  if (a[i] > *max)
+//  *max = a[i];
+//  else if (a[i] < *min)
+//  *min = a[i];
+//  }
+
+    // printf("Enter a message:");
+    // char ch[N];
+    // char ch1[N];
+    // char c;
+    // int i=0;
+    // int tmpi=0;
+    // while(i<N-1&&(c=getchar())!='\n'){
+    //     ch[i]=c;
+    //     i++;
+    // }
+    // char tmp;
+    // while(tmpi<=i){
+    //     ch1[tmpi]=ch[i-tmpi-1];
+    //     tmpi++;
+    // }
+    // printf("%s\n",ch1);
+
+    
 }
