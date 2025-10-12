4c20a9c2d784c49486fb6ce4e882a36f58cef6ae
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-10-12 12:26:28 +0000

    Sun, Oct 12, 2025, 8:26 PM +08:00

--- LearnDS/1.cpp
+++ LearnDS/1.cpp
@@ -4,7 +4,14 @@ using namespace std;
 void bubblesort1A(int a[],int n){
     bool sorted=false;
     while(!sorted){
-        sorted=true
+        sorted=true；
+        for(int i=1;i<n;i++){
+            if(A[i-1]<A[i]){
+                swap(A[i-1],A[i]);
+                sorted=false;
+            }
+        }
+        n--;
     }
 }
 
