891911d9ac6dd0b136dc2f633459cf74260a4c65
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-10-21 11:59:57 +0000

    Tue, Oct 21, 2025, 7:59 PM +08:00

--- LearnAL_New/3.cpp
+++ LearnAL_New/3.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 int main(){
     //HDU 1276
+
+    //Step1
     int t,n;
     cin>>t;
     //测试几轮
@@ -13,15 +15,22 @@ int main(){
         //抓人
         list<int> mylist;
         list<int>::iterator it;
-        //迭代器 
+        //迭代器
         for(int i=1;i<=n;i++){
             mylist.push_back(i);
         }
+        //把数字全压入链表
+
+        //Step2
         while(mylist.size()>3){
+        //条件罢了
             int num=1;
             for(it=mylist.begin();it!=mylist.end;){
+            //淘汰一轮的过程
                 if(num++%k==0){
+                //如果num是k的倍数 这个数就完蛋了
                     it=mylist.erase(it);
+                    //删除这个元素 迭代器指向下一个元素
                 }else{
                     it++;
                 }
