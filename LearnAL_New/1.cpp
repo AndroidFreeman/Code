d854e57f13c167fefc130d0ee2b8aa01d6509709
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-10-21 06:12:54 +0000

    Tue, Oct 21, 2025, 2:12 PM +08:00

---
+++ LearnAL_New/1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printVector(const std::vector<int>& vec) {
+    for (int element : vec) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main(){
+    cout << "Hello" << endl;
+    vector<int> a(10);
+    iota(a.begin(),a.end(),1);
+    printVector(a);
+    vector<string> string_vec(10, "hello");
+    vector<string> b(string_vec.begin(), string_vec.end());
+    a.push_back(199);
+    printVector(a);
+    int size=a.size();
+    bool isEmpty=a.empty();
+    cout<<isEmpty<<endl;
+    a.insert(a.end(),10,5);
+    printVector(a);
+    a.pop_back();
+    printVector(a);
+    a.erase(a.begin()+12,a.begin()+22);
+    printVector(a);
+    vector<int> temp(10);
+    iota(temp.begin(),temp.end(),1);
+    a.insert(a.end(),temp.begin(),temp.end());
+    printVector(a);
+}
