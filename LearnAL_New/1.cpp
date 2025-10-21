aebf40d1f6e7d956768bbfc36777e795cf5d141e
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-10-21 10:03:47 +0000

    Tue, Oct 21, 2025, 6:03 PM +08:00

--- LearnAL_New/1.cpp
+++ LearnAL_New/1.cpp
@@ -38,13 +38,16 @@ int main(){
     sort(a.begin(),a.end());
     printVector(a);
     a.clear();
+
+    //Program HDU4841
     vector<int> table;
     int n,m;
-    while(cin>>n>>m){
+    n=2;
+    m=3;
+    while(1){
         table.clear();
-        for(int i=0;i<2*n;i++){
-            table.push_back(i);
-        }
+        table.resize(2*n);
+        iota(table.begin(),table.end(),0);
         int pos=0;
         for(int i=0;i<n;i++){
             pos=(pos+m-1)%table.size();
@@ -61,7 +64,56 @@ int main(){
             }else{
                 cout<<"B";
             }
-            cout<<endl;
+        }
+        cout<<endl;
+        break;
+    }
+    //Program HDU4841 Using Pointer
+    struct Node{
+        int data;
+        Node* next;
+    }nodes;
+    //Step1
+    Node* head = new Node{0, nullptr};
+    Node* current = head;
+    for (int i = 1; i < 2 * n; i++) {
+        current->next = new Node{i, nullptr};
+        current = current->next;
+    }
+    current->next = head;
+    //Step2
+    Node* prev=current;
+    for(int i=0;i<n;i++){
+        for(int count=0;count<m-1;count++){
+            prev=prev->next;
+        }
+        Node* node_to_delete=prev->next;
+        prev->next=node_to_delete->next;
+        delete node_to_delete;
+    }
+    //Step3
+    head=prev->next;
+    vector<bool> is_survivor(2*n,false);
+    current=head;
+    for(int i=0;i<n;i++){
+        is_survivor[current->data]=true;
+        current=current->next;
+    }
+    for(int i=0;i<2*n;i++){
+        if(is_survivor[i]){
+            cout<<"G";
+        }else{
+            cout<<"B";
         }
     }
+    cout<<endl;
+    //Step4
+    current=head;
+    for(int i=0;i<n;i++){
+        Node* next_node=current->next;
+        delete current;
+        current=next_node;
+    }
+    return 0;
 }
+
---
+++ LearnAL_New/2.cpp
@@ -0,0 +1,42 @@
+//Stack
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    int s_top=s.top();
+    cout<<s_top<<endl;
+    s.pop();
+    s_top=s.top();
+    cout<<s_top<<endl;
+    int s_size=s.size();
+    cout<<s_size<<endl;
+    bool s_isEmpty=s.empty();
+    cout<<s_isEmpty<<endl;
+    //HDU 1062
+    int n;
+    char ch;
+    cin>>n;
+    getchar;
+    while(n--){
+        stack<char> s;
+        while(true){
+            ch=getchar();
+            if(ch==' '||ch=='\n'||ch==EOF){
+                while(!s.empty()){
+                    cout<<s.top();
+                    s.pop();
+                }
+                if(ch=='\n'||ch==EOF){
+                    break;
+                }
+                cout<<" ";
+            }else{
+                s.push(ch);
+            }
+        }
+        cout<<endl;
+    }
+}
