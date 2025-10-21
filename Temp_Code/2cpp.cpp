2cbe424220c5c995549b6dab9ca250bdf3bbc29c
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-10-21 11:32:25 +0000

    Tue, Oct 21, 2025, 7:32 PM +08:00

--- Temp_Code/2cpp.cpp
+++ Temp_Code/2cpp.cpp
@@ -1,19 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string os;
-    getline(cin,os);
+    // string os;
+    // getline(cin,os);
 
-    stack<char> ss;
-    for(char c:os){
-        ss.push(c);
+    // stack<char> ss;
+    // for(char c:os){
+    //     ss.push(c);
+    // }
+
+    // string ts="";
+    // while(!ss.empty()){
+    //     ts+=ss.top();
+    //     ss.pop();
+    // }
+
+    // cout<<ts;
+
+    string input_string;
+    getline(cin,input_string);
+
+    stack<char> trans_string;
+    for(char c:input_string){
+        trans_string.push(c);
     }
 
-    string ts="";
-    while(!ss.empty()){
-        ts+=ss.top();
-        ss.pop();
+    string answer_string="";
+    for(!trans_string.empty()){
+        answer_string+=trans_string.top();
+        trans_string.pop();
     }
 
-    cout<<ts;
+    cout<<trans_string<<endl;
 }
