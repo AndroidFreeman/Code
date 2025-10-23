ec6207efba7d91af123060ade6546be01df2edb4
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-10-23 11:47:34 +0000

    Thu, Oct 23, 2025, 7:47 PM +08:00

--- LearnAL_3/t2.cpp
+++ LearnAL_3/t2.cpp
@@ -6,11 +6,28 @@ int main(){
     stack<char> s;
     string s_in;
     getline(cin,s_in);
-    for(int i=0;i<s_in.size();i++){
-        s.push(s_in[i]);
+    // for(int i=0;i<s_in.size();i++){
+    //     s.push(s_in[i]);
+    // }
+    // for(int i=0;i<s_in.size();i++){
+    //     cout<<s.top();
+    //     s.pop();
+    // }
+    for(char ch:s_in){
+        if(ch==' '){
+            while(!s.empty()){
+                cout<<s.top();
+                s.pop();
+            }
+            cout<<' ';
+        }else{
+            s.push(ch);
+        }
     }
-    for(int i=0;i<s_in.size();i++){
+    while(!s.empty()){
         cout<<s.top();
         s.pop();
     }
+    cout<<endl;
 }
+
