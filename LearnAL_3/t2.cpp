12ff2efef2e7004d1d90dce99eed92e289738d40
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-10-23 11:37:55 +0000

    Thu, Oct 23, 2025, 7:37 PM +08:00

--- LearnAL_3/t2.cpp
+++ LearnAL_3/t2.cpp
@@ -1,8 +1,14 @@
-#include<bits/stdc++/h>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
     stack<char> s;
     string s_in;
-    getchar(cin,s_in);
-    for()
+    getline(cin,s_in);
+    for(int i=0;i<s_in.size();i++){
+        s.push(s_in[i]);
+    }
+    for(int i=0;i<s_in.size();i++){
+        cout<<s.top();
+        s.pop();
+    }
 }
