bd0d4fb240505bf3918411a21556dbfd546acaf5
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-10-28 00:28:47 +0000

    Tue, Oct 28, 2025, 8:28 AM +08:00

--- Luogu/4.cpp
+++ Luogu/4.cpp
@@ -6,17 +6,17 @@ int main(){
     vector<int> left,right;
     string tleft,tright;
     cin>>tleft>>tright;
-    for(int i=tleft.length()-1;i>=0;i++){
+    for(int i=tleft.length()-1;i>=0;i--){
         left.push_back(tleft[i]-'0');
     }
-    for(int i=tright.length()-1;i>=0;i++){
+    for(int i=tright.length()-1;i>=0;i--){
         right.push_back(tright[i]-'0');
     }
 
     //plus
     vector<int> sum;
     int carry=0;
-    for(int i=0;i<left.size()||i<right.size();i++){
+    for(int i=0;i<left.size()||i<right.size();i--){
         if(i<left.size()){
             carry+=left[i];
         }
@@ -26,9 +26,13 @@ int main(){
         sum.push_back(carry%10);
         carry=carry/10;
     }
+    if(carry){
+        sum.push_back(carry);
+    }
 
-
-
-
-
+    //output
+    for(int i=sum.size()-1;i>=0;i--){
+        cout<<sum[i];
+    }
+    cout<<endl;
 }
