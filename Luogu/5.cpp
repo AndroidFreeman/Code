0aa10df2e4a6b590edfb490e483304d196c69b1a
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-10-28 01:30:45 +0000

    Tue, Oct 28, 2025, 9:30 AM +08:00

--- Luogu/5.cpp
+++ Luogu/5.cpp
@@ -1,3 +1,4 @@
+//P1303
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
---
+++ Luogu/6.cpp
@@ -0,0 +1,8 @@
+//P1009
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //input
+    int input,output;
+    
+}
