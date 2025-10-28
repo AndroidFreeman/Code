8cbbe50dfcd29128c664596fe66799a3297ba12d
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-10-28 00:40:35 +0000

    Tue, Oct 28, 2025, 8:40 AM +08:00

---
+++ Luogu/5.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //input
+    string sa,sb;
+    cin>>sa>>sb;
+    if(sa=='0'||sb=='0'){
+        cout<<'0'<<endl;
+        return 0;
+    }
+}
