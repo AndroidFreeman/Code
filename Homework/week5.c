2872cf173dee4cd836b321b0379c9365a54e4b0e
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-11-18 00:12:09 +0000

    Tue, Nov 18, 2025, 8:12 AM +08:00

--- Homework/week5.c
+++ Homework/week5.c
@@ -1,4 +1,17 @@
 #include<stdio.h>
 int main(){
-    
+    //Program1
+    const int M=3;
+    int temp[M];
+    int* p;
+    for(int i=0;i<3;i++){
+        scanf("%d",temp[i]);
+    }
+    for(int i=0;i<3;i++){
+        if(temp[i]>temp[i+1]){
+            *p=temp[i];
+            temp[i+1]=temp[i];
+            temp[i+1]=*p
+        }
+    }
 }
