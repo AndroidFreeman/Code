67d21bc62b4af6d9569ee4b7f596d7042c6c5191
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-11-18 00:25:41 +0000

    Tue, Nov 18, 2025, 8:25 AM +08:00

--- Homework/week5.c
+++ Homework/week5.c
@@ -1,21 +1,34 @@
 #include<stdio.h>
 int main(){
     //Program1
-    const int M=3;
-    int temp[M];
-    for(int i=0;i<2;i++){
-        scanf("%d",&temp[i]);
+    // const int M=3;
+    // int temp[M];
+    // for(int i=0;i<M;i++){
+    //     scanf("%d",&temp[i]);
+    // }
+    // for(int i=0;i<M-1;i++){
+    //     int* p1=&temp[i];
+    //     int* p2=&temp[i+1];
+    //     if(*p1>*p2){
+    //         int swap=*p1;
+    //         *p1=*p2;
+    //         *p2=swap;
+    //     }
+    // }
+    // for(int i=0;i<3;i++){
+    //     printf("%d ",temp[i]);
+    // }
+    // printf("\n");
+
+    //Program2
+    const int N=5;
+    int tmp[N];
+    int total;
+    for(int i=0;i<N;i++){
+        scanf("%d",&tmp[i]);
+        int *p=tmp[N];
+        total+=*p;
     }
-    for(int i=0;i<2;i++){
-        if(temp[i]>temp[i+1]){
-            *p=temp[i];
-            temp[i]=temp[i+1];
-            temp[i+1]=*p
-        }else{
-            continue;
-        }
-    }
-    for(int i=0;i<3;i++){
-        printf("%d",temp[i]);
-    }
+    int ave=total/N;
+    printf("%d",ave);
 }
