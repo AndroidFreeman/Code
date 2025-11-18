267c23da3c720ac95cb98656df7e22657087c199
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-11-18 01:08:30 +0000

    Tue, Nov 18, 2025, 9:08 AM +08:00

--- Homework/week5.c
+++ Homework/week5.c
@@ -33,50 +33,52 @@ int main(){
     // printf("%d",ave);
 
     //Program3
-    const int O=5;
-    int tmp[O];
-    int total=1;
-    for(int i=0;i<O;i++){
-        scanf("%d",&tmp[i]);
-        int *p=&tmp[i];
-        total=total*(*p);
-    }
-    printf("%d",total);
+    // const int O=5;
+    // int tmp[O];
+    // int total=1;
+    // for(int i=0;i<O;i++){
+    //     scanf("%d",&tmp[i]);
+    //     int *p=&tmp[i];
+    //     total=total*(*p);
+    // }
+    // printf("%d",total);
 
     //Program4
-    int Q;
-    scanf("%d", &Q);
-    int tmpp[Q];
-    int* p;
-    int temp;
-    for (p = tmpp; p < tmpp + Q; p++) {
-        scanf("%d", p);
-    }
+    // int Q;
+    // scanf("%d", &Q);
+    // int tmpp[Q];
+    // int* p;
+    // int temp;
+    // for (p = tmpp; p < tmpp + Q; p++) {
+    //     scanf("%d", p);
+    // }
 
-    int* p_max = tmpp;
-    for (p = tmpp + 1; p < tmpp + Q; p++) {
-        if (*p > *p_max) {
-            p_max = p;
-        }
-    }
-    temp = *tmpp;
-    *tmpp = *p_max;
-    *p_max = temp;
-    int* p_min = tmpp + 1;
-    for (p = tmpp + 2; p < tmpp + Q; p++) {
-        if (*p < *p_min) {
-            p_min = p;
-        }
-    }
-    int* p_last = tmpp + Q - 1;
-    temp = *p_last;
-    *p_last = *p_min;
-    *p_min = temp;
+    // int* p_max = tmpp;
+    // for (p = tmpp + 1; p < tmpp + Q; p++) {
+    //     if (*p > *p_max) {
+    //         p_max = p;
+    //     }
+    // }
+    // temp = *tmpp;
+    // *tmpp = *p_max;
+    // *p_max = temp;
+    // int* p_min = tmpp + 1;
+    // for (p = tmpp + 2; p < tmpp + Q; p++) {
+    //     if (*p < *p_min) {
+    //         p_min = p;
+    //     }
+    // }
+    // int* p_last = tmpp + Q - 1;
+    // temp = *p_last;
+    // *p_last = *p_min;
+    // *p_min = temp;
 
-    for (p = tmpp; p < tmpp + Q; p++) {
-        printf("%d ", *p);
-    }
-    printf("\n");
+    // for (p = tmpp; p < tmpp + Q; p++) {
+    //     printf("%d ", *p);
+    // }
+    // printf("\n");
 
     //Program5
+    int
+
 }
