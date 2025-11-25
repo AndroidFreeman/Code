af10b343f3c07f62af2b69984f1f7cf8c435e7a5
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-11-25 00:36:32 +0000

    Tue, Nov 25, 2025, 8:36 AM +08:00

--- Homework/week6.c
+++ Homework/week6.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
-
 //Program1
-
 double Fun1T(int number){
     double answer=1;
     for(int i=number;i>0;i--){
@@ -9,7 +7,6 @@ double Fun1T(int number){
     }
     return answer;
 }
-
 double Fun1(int input_1,int input_2){
     double upper=Fun1T(input_1);
     double down1=Fun1T(input_2);
@@ -17,18 +14,12 @@ double Fun1(int input_1,int input_2){
     double answer=(upper/(down1*down2));
     return answer;
 }
-
-
 //Program2
-
 int Fun2(int n){
     if(n==0||n==1) return 1;
     return n*Fun2(n-1);
 }
-
-
 //Program3
-
 double Fun3(int n){
     double result=0.0;
     double fm=0.0;
@@ -38,10 +29,7 @@ double Fun3(int n){
     }
     return result;
 }
-
-
 //Program4
-
 int Fun4(int a,int b){
     int ones,tens,huns,thos,res;
     ones=a%10;tens=b/10;
@@ -49,47 +37,40 @@ int Fun4(int a,int b){
     res=ones+tens*10+huns*100+thos*1000;
     return res;
 }
-
-
 //Program5
-
 void Fun5_1(int* arr,int n){
     for(int i=0;i<n;i++){
         scanf("%d",arr+i);
     }
 }
-
 void Fun5_2(int* arr,int n){
     int temp;
     int *pMin=arr;
-    for (int i = 1; i < n; i++) {
-        if (*(arr + i) < *pMin) {
-            pMin = arr + i;
+    for(int i=1;i<n;i++){
+        if(*(arr+i)<*pMin){
+            pMin=arr+i;
         }
     }
-    temp = *arr;
-    *arr = *pMin;
-    *pMin = temp;
-
-    int *pMax = arr;
-    for (int i = 1; i < n; i++) {
-        if (*(arr + i) > *pMax) {
-            pMax = arr + i;
+    temp=*arr;
+    *arr=*pMin;
+    *pMin=temp;
+    int *pMax=arr;
+    for(int i=1;i<n;i++){
+        if(*(arr+i)>*pMax){
+            pMax=arr+i;
         }
     }
-    int *pLast = arr + n - 1;
-    temp = *pLast;
-    *pLast = *pMax;
-    *pMax = temp;
+    int *pLast=arr+n-1;
+    temp=*pLast;
+    *pLast=*pMax;
+    *pMax=temp;
 }
-
 void Fun5_3(int *arr,int n){
     for(int i=0;i<n;i++){
         printf("%d ",*(arr+i));
     }
     printf("\n");
 }
-
 //Program6
 void Fun6(int M,int* answer_num,int input_arr[]){
     *answer_num=0;
@@ -122,7 +103,6 @@ int main(){
     }
     double ans_1=Fun1(bigger,smaller);
     printf("%lf\n",ans_1);
-
     //Program2
     int i,n,sum=0;
     scanf("%d",&n);
@@ -130,27 +110,22 @@ int main(){
         sum+=Fun2(i);
     }
     printf("%d\n",sum);
-
-
     //Program3
     int nn;
     scanf("%d",&nn);
     double result3=Fun3(nn);
     printf("%lf\n",result3);
-
     //Program4
     int j_p4,k_p4;
     scanf("%d %d",&j_p4,&k_p4);
     double result4=Fun4(j_p4,k_p4);
     printf("%.0lf\n",result4);
-
     //Program5
     const int N=10;
     int arr[N];
     Fun5_1(arr,N);
     Fun5_2(arr,N);
     Fun5_3(arr,N);
-
     //Program6
     const int M=100;
     int answer_num=0;
