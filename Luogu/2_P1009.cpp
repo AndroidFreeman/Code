e0c41c1bf7bdb98843117e5ee570390871cafb34
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-11-26 07:01:04 +0000

    Wed, Nov 26, 2025, 3:01 PM +08:00

--- Luogu/2_P1009.cpp
+++ Luogu/2_P1009.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    long long m;
-    long long n;
+    long long m,n,rec,sqr;
     cin>>n>>m;
-    for(int i=1;)
+    for(int i=1;i<=n;i++){
+        for(int j=1;i<=m;j++){
+            
+        }
+    }
 }
