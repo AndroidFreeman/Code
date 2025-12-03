d72fe7c6df6390ae19b0fb5663dc6903fb301f11
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-12-03 11:05:36 +0000

    Wed, Dec 3, 2025, 7:05 PM +08:00

--- Homework/test1.c
+++ Homework/test1.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 int main(){
-    int n;
-    scanf("%d",&n);
+    //Program1
+    int n=20;
     int arr[n];
+    arr[0]=1;
+    arr[1]=1;
+    for(int i=2;i<n;i++){
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    for(int i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+
+    //Program2
     
 }
+
+
