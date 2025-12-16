376e8f37eb8c14ef21847070980c96057be744c9
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-12-16 07:42:09 +0000

    Tue, Dec 16, 2025, 3:42 PM +08:00

---
+++ Aha/t.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+}LNode,*LinkList;
+void Insert(){
+
+}
+void Init(LinkList *h){
+
+}
+void Print(LinkList h){
+    LNode *p;
+    for(p=h->next;)
+}
+int main(){
+    LinkList head;
+    LNode *p,*q;
+    Init(head);
+}
