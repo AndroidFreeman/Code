d45afc6d3547327bdf48146ad9362641c10a5864
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-12-19 01:22:17 +0000

    Fri, Dec 19, 2025, 9:22 AM +08:00

--- Temp_Code/_1.cpp
+++ Temp_Code/_1.cpp
@@ -7,9 +7,11 @@ ListNode *n0=new ListNode(1);
 ListNode *n1=new ListNode(3);
 ListNode *n2=new ListNode(2);
 ListNode *n3=new ListNode(5);
-
-
-
+ListNode *n4=new ListNode(4);
+n0->next=n1;
+n1->next=n2;
+n2->next=n3;
+n3->next=n4;
 void insert(ListNode *n0,ListNode *P){
     ListNode *n1=n0->next;
     P->next=n1;
