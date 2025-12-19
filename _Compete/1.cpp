a2982e9150a73b4a416bd8d42fccd7e202efb102
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-12-19 00:11:59 +0000

    Fri, Dec 19, 2025, 8:11 AM +08:00

--- _Compete/1.cpp
+++ _Compete/1.cpp
@@ -3,5 +3,16 @@ using namespace std;
 int main(){
     int numX=10;
     int numY=90;
-    
+    for(int tick=1;tick<=120;tick++){
+        if(tick%2==1){
+            numY=numY-numX;
+        }
+        if(tick%4==0){
+            numY=numY*2;
+        }
+        if(tick%6==0){
+            numX=numX*2;
+        }
+    }
+    cout<<numY<<endl;
 }
