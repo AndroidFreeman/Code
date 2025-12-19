a47d1ea6c9cfb6ec2c50e94c9263d0d311cf575d
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-12-19 00:31:57 +0000

    Fri, Dec 19, 2025, 8:31 AM +08:00

--- _Compete/3.cpp
+++ _Compete/3.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    for(int p=20;p>=1;p++){
+        int p1=p%2;
+        int p2=p1%2;
+        int p3=p2%2;
+        if(p3==0) continue;
+        int p4=p3%2;
+        if(p4==0){
+            cout<<p1<<p2<<p3<<p4;
+        }
+    }
 }
