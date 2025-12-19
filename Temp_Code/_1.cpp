a129f6ec047023e83162112668856e7f284b4cd5
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-12-19 01:20:35 +0000

    Fri, Dec 19, 2025, 9:20 AM +08:00

--- Temp_Code/_1.cpp
+++ Temp_Code/_1.cpp
@@ -3,6 +3,13 @@ struct ListNode{
     ListNode *next;
     ListNode(int x):val(x),next(nullptr){}
 };
+ListNode *n0=new ListNode(1);
+ListNode *n1=new ListNode(3);
+ListNode *n2=new ListNode(2);
+ListNode *n3=new ListNode(5);
+
+
+
 void insert(ListNode *n0,ListNode *P){
     ListNode *n1=n0->next;
     P->next=n1;
