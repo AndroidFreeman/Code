807bf919e5b74c1350c33de61cc61fc199bb8ca7
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-12-19 00:43:04 +0000

    Fri, Dec 19, 2025, 8:43 AM +08:00

--- _Compete/3.cpp
+++ _Compete/3.cpp
@@ -1,18 +1,46 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     // a, b, c, d 分别代表四轮喝酒时的人数
+//     // a 为初始人数，不超过 20
+//     for (int a = 20; a >= 4; a--) {
+//         for (int b = a - 1; b >= 3; b--) {
+//             for (int c = b - 1; c >= 2; c--) {
+//                 for (int d = c - 1; d >= 1; d--) {
+//                     // 使用整数运算判断 1/a + 1/b + 1/c + 1/d == 1
+//                     long long left = (long long)b * c * d + (long long)a * c * d +
+//                                      (long long)a * b * d + (long long)a * b * c;
+//                     long long right = (long long)a * b * c * d;
+//                     if (left == right) {
+//                         cout << a << "," << b << "," << c << "," << d << ",0" << endl;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+#include<iostream>
 using namespace std;
-int main() {
-    // a, b, c, d 分别代表四轮喝酒时的人数
-    // a 为初始人数，不超过 20
-    for (int a = 20; a >= 4; a--) {
-        for (int b = a - 1; b >= 3; b--) {
-            for (int c = b - 1; c >= 2; c--) {
-                for (int d = c - 1; d >= 1; d--) {
-                    // 使用整数运算判断 1/a + 1/b + 1/c + 1/d == 1
-                    long long left = (long long)b * c * d + (long long)a * c * d +
-                                     (long long)a * b * d + (long long)a * b * c;
-                    long long right = (long long)a * b * c * d;
-                    if (left == right) {
-                        cout << a << "," << b << "," << c << "," << d << ",0" << endl;
+int main(){
+    for(int a=20;a>=4;a--){
+        for(int b=a-1;b>=3;b--){
+            for(int c=b-1;c>=2;c--){
+                for(int d=c-1;d>=1;d--){
+                    int left=b*c*d+a*b*c+a*b*d+a*c*d;
+                    int right=a*b*c*d;
+                    if(left==right){
+                        cout<<a<<' '<<b<<' '<<c<<' '<<d<<' '<<endl;
                     }
                 }
             }
