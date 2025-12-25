a98a38a5f9e1fbac3344e98aa503e723e9bbe71b
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-12-25 11:04:57 +0000

    Thu, Dec 25, 2025, 7:04 PM +08:00

--- LearnPY/5.py
+++ LearnPY/5.py
@@ -3,3 +3,24 @@ print(spam[0])
 print(spam)
 print(spam[-1])
 print(spam[1:3])
+print(spam[0:-1])
+print(spam[:2])
+print(len(spam))
+spam[1]=91
+print(spam)
+tmp=[91,69]
+print(spam+tmp)
+temp=tmp
+print(temp)
+del spam[0]
+print(spam)
+catNames=[]
+while True:
+    print('Enter '+str(len(catNames)+1))
+    name=input()
+    if name=='':
+        break
+    catNames=catNames+[name]
+print('The cat names are:')
+for name in catNames:
+    print(name+' ')
