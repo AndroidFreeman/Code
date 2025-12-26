6db886a68608e1b3dd7ea1eb12a2da9af0fd387e
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-12-26 11:28:47 +0000

    Fri, Dec 26, 2025, 7:28 PM +08:00

--- HelloAL/1.cpp
+++ HelloAL/1.cpp
@@ -1,6 +1,12 @@
 //array
 #include<bits/stdc++.h>
 using namespace std;
+void printArr(int *nums,int size){
+    for(int i=0;i<size;i++){
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+}
 int randomAccess(int *nums,int size){
     int randomIndex=rand()%size;
     int randomNum=nums[randomIndex];
@@ -23,17 +29,9 @@ int main(){
     int* arr1=new int[5];
     int* nums1=new int[5]{1,3,2,5,4};
     int ramdomNum=randomAccess(arr,5);
-    for(int i=0;i<5;i++){
-        cout<<arr[i]<<" ";
-    }
+    printArr(arr,10);
     insert(arr,10,5,1);
-    for(int i=0;i<10;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-    remove(arr,10,5);
-    for(int i=0;i<10;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+    printArr(arr,10);
+    remove(arr,10,1);
+    printArr(arr,10);
 }
