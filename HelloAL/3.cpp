a0b98ffceefd7aba95dbc631e1ed1544a2409f28
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2025-12-26 12:19:13 +0000

    Fri, Dec 26, 2025, 8:19 PM +08:00

--- HelloAL/3.cpp
+++ HelloAL/3.cpp
@@ -9,4 +9,15 @@ int main(){
     nums.push_back(5);
     nums.insert(nums.begin()+3,6);
     nums.erase(nums.begin()+3);
+    int count=0;
+    for(int i=0;i<nums.size();i++){
+        count+=nums[i];
+    }
+    count=0;
+    for(int num:nums){
+        count+=num;
+    }
+    vector<int> nums1={6,8,7,10,9};
+    nums.insert(nums.end(),nums1.begin(),nums1.end());
+    sort(nums.begin(),nums.end());
 }
