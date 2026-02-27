0cfafee1d597b5260f9742bdb07be586350277c9
AndroidFreeman <3077128239@qq.com>
2026-02-27 21:19:04 +0800

    Win sync 2026-02-27 21:19:04

--- _Rebuild/2025-02-27_2.cpp
+++ _Rebuild/2025-02-27_2.cpp
@@ -1,18 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
     vector<vector<int>> ps;
     vector<int> s;
     s.push_back(1);
     ps.push_back(s);
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= n; i++)
+    {
         vector<vector<int>> ps1;
-        for (int j = 0; j < ps.size(); j++) {
+        for (int j = 0; j < ps.size(); j++)
+        {
             vector<int> cur = ps[j];
-            for (int k = 0; k <= cur.size(); k++) {
+            for (int k = 0; k <= cur.size(); k++)
+            {
                 vector<int> next_p = cur;
                 next_p.insert(next_p.begin() + k, i);
                 ps1.push_back(next_p);
@@ -20,11 +24,12 @@ int main() {
         }
         ps = ps1;
     }
-    for (int i = 0; i < ps.size(); i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < ps.size(); i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             cout << ps[i][j] << (j == n - 1 ? "" : " ");
         }
         cout << endl;
     }
-    return 0;
 }
--- _Rebuild/2025-02-27_3.cpp
+++ _Rebuild/2025-02-27_3.cpp
@@ -1,29 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
     int a[15];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         a[i] = i + 1;
     }
-    while (true) {
-        for (int i = 0; i < n; i++) {
+    while (true)
+    {
+        for (int i = 0; i < n; i++)
+        {
             cout << a[i] << (i == n - 1 ? "" : " ");
         }
         cout << endl;
         int i = n - 2;
-        while (i >= 0 && a[i] >= a[i + 1]) {
+        while (i >= 0 && a[i] >= a[i + 1])
+        {
             i--;
         }
-        if (i < 0) break;
+        if (i < 0)
+            break;
         int j = n - 1;
-        while (a[j] <= a[i]) {
+        while (a[j] <= a[i])
+        {
             j--;
         }
         swap(a[i], a[j]);
         reverse(a + i + 1, a + n);
     }
-    return 0;
 }
