ce271e610a72603842222fcf63ca2a0af9236dde
Android_Freeman <3077128239@qq.com>
2026-03-07 16:50:36 +0800

    Sync: 2026-03-07 16:50:36

--- Luogu/Traning_107/8_P1116.cpp
+++ Luogu/Traning_107/8_P1116.cpp
@@ -6,3 +6,14 @@
  * @LastEditTime: 2026-03-07 16:50:13
  * @FilePath: /Code_Sync/Luogu/Traning_107/8_P1116.cpp
  */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+
+    return 0;
+}
