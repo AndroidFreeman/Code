4dc50a7e763d2947321832831985fb72ef5057ab
Android_Freeman <3077128239@qq.com>
2026-03-10 22:08:44 +0800

    Rebuild: Luogu/Training_108/4_P1036.cpp (2026-03-10 22:08:44) @ArchLinux

---
+++ Luogu/Training_108/4_P1036.cpp
@@ -0,0 +1,26 @@
+/*
+ * @Date: 2026-03-08 21:17:25
+ * @Github: https://github.com/AndroidFreeman
+ * BTW, I use Arch 
+ * @Author: Android_Freeman
+ * @LastEditTime: 2026-03-08 21:21:20
+ * @FilePath: /Code_Sync/Luogu/Training_108/4_P1036.cpp
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int total, choose, sum, answer;
+    if (!(cin >> total >> choose)) return -1;
+    vector<int> number(total, 0);
+    for (int i = 0; i < total; i++) {
+        cin >> number[i];
+    }
+    for (int i = 0; i < total; i++) {
+    }
+
+    return 0;
+}
