aa5d981964235606380b9723ef87224d83a55a80
Android_Freeman <3077128239@qq.com>
2026-03-10 23:02:21 +0800

    Rebuild: Luogu/Training_109/ (2026-03-10 23:02:21) @ArchLinux

---
+++ Luogu/Training_109/1_P1255.cpp
@@ -0,0 +1,23 @@
+/*
+ * @Date: 2026-03-10 22:53:32
+ * @Github: https://github.com/AndroidFreeman
+ * BTW, I use Arch 
+ * @Author: Android_Freeman
+ * @LastEditTime: 2026-03-10 22:57:03
+ * @FilePath: /Code_Sync/Luogu/Training_109/1_P1255.cpp
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int step;
+    int method=0;
+    if(!(cin>>step)) return -1;
+    
+    
+
+    return 0;
+}
