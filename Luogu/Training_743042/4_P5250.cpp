198bc9ec6e2a6e989a08b542795badde46a3d735
Android_Freeman <3077128239@qq.com>
2026-03-13 18:01:12 +0800

    Rebuild: Luogu/Training_743042/4_P5250.cpp (2026-03-13 18:01:12) @ArchLinux

---
+++ Luogu/Training_743042/4_P5250.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Date: 2026-03-13 17:30:37
+ * @Github: https://github.com/AndroidFreeman
+ * BTW, I use Arch 
+ * @Author: Android_Freeman
+ * @LastEditTime: 2026-03-13 17:47:00
+ * @FilePath: /Code_Sync/Luogu/Training_743042/4_P5250.cpp
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    set<int> s;
+    
+
+    return 0;
+}
