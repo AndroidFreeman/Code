2f04c9982f508b0c1a716b7f34a1028260727a8d
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2026-03-18 09:37:41 +0000

    feat: sync from cloud

--- Luogu/Training_743042/4_P5250.cpp
+++ Luogu/Training_743042/4_P5250.cpp
@@ -3,7 +3,7 @@
  * @Github: https://github.com/AndroidFreeman
  * BTW, I use Arch 
  * @Author: Android_Freeman
- * @LastEditTime: 2026-03-18 16:37:51
+ * @LastEditTime: 2026-03-18 09:27:36
  * @FilePath: /Code_Sync/Luogu/Training_743042/4_P5250.cpp
  */
 #include <bits/stdc++.h>
