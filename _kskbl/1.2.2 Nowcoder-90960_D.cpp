34842354907a6840c92ef9580cebdcca3bf27828
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2026-03-22 12:29:03 +0000

    Rebuild: _kskbl/1.2.2 Nowcoder-90960_D.cpp (12:29:03) @Codespace

--- _kskbl/1.2.2 Nowcoder-90960_D.cpp
+++ _kskbl/1.2.2 Nowcoder-90960_D.cpp
@@ -3,6 +3,23 @@
  * @Github: https://github.com/AndroidFreeman
  * Now, I use my Codespace
  * @Author: Android_Freeman
- * @LastEditTime: 2026-03-22 12:21:49
+ * @LastEditTime: 2026-03-22 12:25:59
  * @FilePath: /Code/_kskbl/1.2.2 Nowcoder-90960_D.cpp
  */
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int a = 0, b = 0, x = 0, y = 0;
+    cin >> a >> b >> x >> y;
+    int step = 0;
+
+    if (x > y) {
+        int temp = max(a, b);
+        int temp1;
+        if (temp % x != 0) temp1 = 1;
+        cout <<temp/x+temp1<<endl;
+    }
+}
