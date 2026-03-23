251f97ac9aad2f00b0b74996d07f2022fe17b548
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2026-03-23 09:16:06 +0000

    Rebuild: LearnDS_HBPU/0.1_myList.cpp (09:16:06) @Codespace

--- LearnDS_HBPU/0.1_myList.cpp
+++ LearnDS_HBPU/0.1_myList.cpp
@@ -3,7 +3,7 @@
  * @Github: https://github.com/AndroidFreeman
  * Now, I use my Codespace
  * @Author: Android_Freeman
- * @LastEditTime: 2026-03-23 09:06:45
+ * @LastEditTime: 2026-03-23 09:15:06
  * @FilePath: /Code/LearnDS_HBPU/0.1_myList.cpp
  */
 #include <bits/stdc++.h>
@@ -39,9 +39,8 @@ string GetElem(SqList L, int i, ElemType& e) {
 }
 
 int LocateElem(SqList L, ElemType e) {
-    for (int i = 0; i < L.length; i++) {
+    for (int i = 0; i < L.length; i++)
         if (L.elem[i].ISBN == e.ISBN) return i + 1;
-    }
     return 0;
 }
 
@@ -82,7 +81,7 @@ int main() {
         cout << "5. 显示所有书籍" << endl;
         cout << "0. 退出系统" << endl;
         cout << "请输入指令: ";
-        
+
         if (!(cin >> choice)) break;
 
         if (choice == 1) {
@@ -97,43 +96,41 @@ int main() {
             cout << "请输入价格: ";
             cin >> e.price;
             cout << "操作结果: " << ListInsert(L, i, e) << endl;
-        } 
-        else if (choice == 2) {
+        } else if (choice == 2) {
             int i;
             cout << "请输入要删除的位置: ";
             cin >> i;
             cout << "操作结果: " << ListDelete(L, i) << endl;
-        } 
-        else if (choice == 3) {
+        } else if (choice == 3) {
             ElemType e;
             cout << "请输入要查找的 ISBN: ";
             cin >> e.ISBN;
             int pos = LocateElem(L, e);
-            if (pos) cout << "找到该书，在第 " << pos << " 个位置" << endl;
-            else cout << "未找到相关书籍" << endl;
-        } 
-        else if (choice == 4) {
+            if (pos)
+                cout << "找到该书，在第 " << pos << " 个位置" << endl;
+            else
+                cout << "未找到相关书籍" << endl;
+        } else if (choice == 4) {
             int i;
             ElemType e;
             cout << "请输入位置: ";
             cin >> i;
             if (GetElem(L, i, e) == "OK") {
-                cout << "书籍信息: " << e.ISBN << " | " << e.name << " | " << e.price << endl;
+                cout << "书籍信息: " << e.ISBN << " | " << e.name << " | "
+                     << e.price << endl;
             } else {
                 cout << "位置无效" << endl;
             }
-        } 
-        else if (choice == 5) {
+        } else if (choice == 5) {
             cout << "当前共有 " << L.length << " 本书：" << endl;
             for (int j = 0; j < L.length; j++) {
-                cout << "[" << j + 1 << "] " << L.elem[j].ISBN << "\t" << L.elem[j].name << "\t" << L.elem[j].price << "\n";
+                cout << "[" << j + 1 << "] " << L.elem[j].ISBN << "\t"
+                     << L.elem[j].name << "\t" << L.elem[j].price << "\n";
             }
-        } 
-        else if (choice == 0) {
+        } else if (choice == 0) {
             cout << "程序已退出" << endl;
             break;
-        } 
-        else {
+        } else {
             cout << "无效指令，请重新输入" << endl;
         }
     }
