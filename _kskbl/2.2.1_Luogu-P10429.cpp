7ed2049a06e239ad039b523d55b202a1cbc16eb4
Android_Freeman <3077128239@qq.com>
2026-03-24 23:48:47 +0800

    Rebuild: _kskbl/2.2.1_Luogu-P10429.cpp (2026-03-24 23:48:47) @ArchLinux

---
+++ _kskbl/2.2.1_Luogu-P10429.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Date: 2026-03-24 21:09:42
+ * @Github: https://github.com/AndroidFreeman
+ * BTW, I use Arch 
+ * @Author: Android_Freeman
+ * @LastEditTime: 2026-03-24 21:09:43
+ * @FilePath: /Code_Sync/_kskbl/2.2.1_Luogu-P10429.cpp
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+
+    return 0;
+}
