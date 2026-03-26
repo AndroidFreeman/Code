8c374c5c8c654b93fcfa06c493a4e916a0a9a9a2
Android_Freeman <3077128239@qq.com>
2026-03-26 23:41:48 +0800

    Rebuild: _CProgram/teacherID.cpp (2026-03-26 23:41:48) @ArchLinux

--- _CProgram/teacherID.cpp
+++ _CProgram/teacherID.cpp
@@ -3,28 +3,26 @@
  * @Github: https://github.com/AndroidFreeman
  * Now, I use my Codespace
  * @Author: Android_Freeman
- * @LastEditTime: 2026-03-26 23:36:38
- * @FilePath: /Code/_CProgram/teacherID.cpp
+ * @LastEditTime: 2026-03-26 23:41:44
+ * @FilePath: /Code_Sync/_CProgram/teacherID.cpp
  */
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-
     if (argc < 6) {
-
         printf(
-         "{\"status\":\"error\", \"message\":\"参数不足!需要:教师ID "
+            "{\"status\":\"error\", \"message\":\"参数不足!需要:教师ID "
             "姓名 性别 所属学院  登录密码\"}\n");
-        return 1;  
+        return 1;
     }
- 
+
     // ---- 第二步：接收前端传来的数据 ----
-    char* name = argv[1];  
+    char* name = argv[1];
     char* sex = argv[2];
-    char*IDnum=argv[3];   
-    char*school=argv[4];
-    char*password=argv[5];
+    char* IDnum = argv[3];
+    char* school = argv[4];
+    char* password = argv[5];
 
     // ---- 第三步：执行你的逻辑（比如写文件、计算等） ----
     // 这里是你的核心代码，比如把数据存进 CSV 文件
@@ -34,15 +32,13 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    fprintf(file, "%s,%s,%s,%s,%s\n", name, sex,IDnum,school,password);
+    fprintf(file, "%s,%s,%s,%s,%s\n", name, sex, IDnum, school, password);
     fclose(file);  // 记得关文件
-
 
     printf(
         "{\"status\":\"success\", \"message\":\"教师注册成功\", "
         "\"IDnum\":\"%s\", \"name\":\"%s\", \"school\":\"%s\"}\n",
         IDnum, name, school);
 
-
-    return 0;  
+    return 0;
 }
