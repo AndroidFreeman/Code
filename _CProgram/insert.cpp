e1b7fb9d8fad4f37e2ef6f55bf41868d956875ed
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2026-03-27 22:25:57 +0800

    Rebuild: _CProgram/insert.cpp (22:25:57) @Codespace

--- _CProgram/insert.cpp
+++ _CProgram/insert.cpp
@@ -3,7 +3,7 @@
  * @Github: https://github.com/AndroidFreeman
  * Now, I use my Codespace
  * @Author: Android_Freeman
- * @LastEditTime: 2026-03-26 22:23:14
+ * @LastEditTime: 2026-03-27 14:27:57
  * @FilePath: /Code/_CProgram/insert.cpp
  */
 
@@ -19,18 +19,20 @@ using namespace std;
 int main(int argc, char* argv[]) {
     // ---- 第一步：检查参数够不够 ----
     // argv[0] 是程序名自己，argv[1] 是第一个参数，以此类推
-    if (argc < 3) {
+    if (argc < 6) {
         // 如果参数不够，打印一个错误的 JSON 告诉前端
         printf(
-            "{\"status\":\"error\", \"message\":\"参数不足！需要：姓名 "
-            "年龄\"}\n");
+            "{\"status\":\"error\", \"message\":\"参数不足!需要:学生ID"
+        "姓名  性别 所属学院 登录密码\"}\n");
         return 1;  // 返回 1 表示出错了
     }
 
     // ---- 第二步：接收前端传来的数据 ----
     char* name = argv[1];  // 比如 "张三"
-    char* age = argv[2];   // 比如 "20"
-
+    char* sex= argv[2];   // 比如 "20"
+    char*IDnum=argv[3];
+    char*school=argv[4];
+    char*password=argv[5];
     // ---- 第三步：执行你的逻辑（比如写文件、计算等） ----
     // 这里是你的核心代码，比如把数据存进 CSV 文件
     FILE* file = fopen("database.csv", "a");  // 以“追加”模式打开文件
@@ -39,17 +41,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // 写入一行：姓名,年龄
-    fprintf(file, "%s,%s\n", name, age);
+    // 写入信息
+    fprintf(file, "%s,%s,%s,%s,%s\n", name, sex,IDnum,school,password);
     fclose(file);  // 记得关文件
 
     // ---- 第四步：大功告成，反馈给前端 ----
     // 💡 注意：JSON 里的双引号在 C 语言里要写成 \"
     // 这一行打印出来，Flutter 就能抓到并显示在 UI 上
     printf(
-        "{\"status\":\"success\", \"message\":\"保存成功\", "
-        "\"saved_name\":\"%s\"}\n",
-        name);
+        "{\"status\":\"success\", \"message\":\"学生注册成功\", "
+        "\"IDnum\":\"%s\",\"name\":\"%s\",\"school\":\"%s\"}\n",
+       IDnum, name,school);
 
     return 0;  // 返回 0 表示完美执行
 }
