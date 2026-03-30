c8da3e74d980d84dfa7735cf1aad21a21878bce3
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2026-03-30 17:18:27 +0800

    Rebuild: LearnDS_HBPU/week1.cpp (17:18:27) @Codespace

---
+++ LearnDS_HBPU/week1.cpp
@@ -0,0 +1,34 @@
+/*
+ * @Date: 2026-03-30 17:14:28
+ * @Github: https://github.com/AndroidFreeman
+ * Now, I use my Codespace
+ * @Author: Android_Freeman
+ * @LastEditTime: 2026-03-30 17:17:40
+ * @FilePath: /Code/LearnDS_HBPU/week1.cpp
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+const int BaseLine=20;
+
+struct ElemType{
+    int number;
+    char name[BaseLine];
+    char sex[BaseLine];
+    int tel[BaseLine];
+    int qq[BaseLine];
+};
+
+struct SqList{
+    ElemType* elem;
+    int length;
+    int list_size;
+};
+
+bool InitList_Sq(SqList &L){
+    
+}
+
+int main(){
+
+}
