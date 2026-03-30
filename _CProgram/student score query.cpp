a8b1b08b88f602f813fa5880c858217cde93e81a
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2026-03-30 17:18:28 +0800

    Rebuild: _CProgram/student score query.cpp (17:18:28) @Codespace

---
+++ _CProgram/student score query.cpp
@@ -0,0 +1,21 @@
+/*
+ * @Date: 2026-03-30 15:29:45
+ * @Github: https://github.com/AndroidFreeman
+ * Now, I use my Codespace
+ * @Author: Android_Freeman
+ * @LastEditTime: 2026-03-30 15:35:29
+ * @FilePath: /Code/_CProgram/student score query.cpp
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    if(argc<2)
+    {
+        printf("{\"status\":\"error\",\"message\":\"参数不足！需要：学号\"}\n");
+        return 1;
+    }
+    char
+
+        return 0;
+}
