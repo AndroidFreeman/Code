46872d93e822ec31826fe0ca91ce0f4d3b8a5e43
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2026-04-01 21:21:27 +0800

    Rebuild: _CProgram/Student Notice Borad.cpp (21:21:27) @Codespace

--- _CProgram/Student Notice Borad.cpp
+++ _CProgram/Student Notice Borad.cpp
@@ -3,36 +3,56 @@
  * @Github: https://github.com/AndroidFreeman
  * Now, I use my Codespace
  * @Author: Android_Freeman
- * @LastEditTime: 2026-03-30 15:07:19
+ * @LastEditTime: 2026-03-31 18:54:29
  * @FilePath: /Code/_CProgram/Student Notice Borad.cpp
  */
 #include <bits/stdc++.h>
 using namespace std;
 int main(int argc,char*argv[])
 {
-    if(argc<4)
-    {
-        printf(
-            "{\"status\":\"error\", \"message\":\"参数不足!需要:教师ID "
-            "学号  姓名  公告内容\"}\n");
-        return 1;
-    }
-    char *id=argv[1];
-    char*name=argv[2];
-    char*content=argv[3];
-     FILE*file=fopen("note_board.csv","a");
-     if(file==NULL)
+     if(argc==1)
      {
-        printf("{\"status\":\"error\",\"message\":\"无法打开公告文件\"}\n");
+          FILE*file=fopen("notice.csv","r");
+          if(file==NULL)
+          {
+          printf("{\"status\":\"error\",\"message\":\"暂无公告\"}\n");
+            return 1;
+          }
      
-         return 1;
-     } 
+     char publisher[200];
+     char title[200];
+     char content[1000];
+     int first=1;
 
-     fprintf(file,"%s,%s,%s\n",id,name,content);
-
+     printf("{\"status\":\"success\",\"message\":\"获取公告成功\",\"notice_list\":[\n");
+     
+     //按csv读取
+    while (fscanf (file, "%199[^,],%199[^,],%999[^\n]\n", publisher, title, content) ==3){
+           if (!first) printf(",\n");
+            printf("{\"publisher\":\"%s\",\"title\":\"%s\",\"content\":\"%s\"}", publisher, title, content);
+            first = 0;
+     }
+    printf("\n]}\n");
     fclose(file);
-    printf("{\"status\":\"success\",\"message\":\"公告发布成功\","
-        "\"student_id\":\"%s\",\"student_name\":\"%s\"}\n",id,name);
-     return 0;
-}
+    return 0;
+    }
+    if(argc>4){
+     FILE*file=fopen("notice.csv","a");
+     if(file==NULL)
+      {
+          printf("{\"status\":\"error\",\"message\":\"文件打开失败\"}\n");
+          return 1;
+      }
+      char*pulisher=argv[1];
+      char*title=argv[2];
+      char*content=argv[3];
+      fprintf(file,"%s,%s,%s\n",pulisher,title,content);
 
+      printf("{\"status\":\"error\",\"message\":\"公告发布成功\",\title\":\"%s\"}\n",title);
+      fclose(file);
+      return 0;
+    }
+    //参数错误提示
+    printf("{\"status\":\"error\",\"message\":\"参数不足！需要：发布人 标题 内容}\n");
+    return 1;
+}
