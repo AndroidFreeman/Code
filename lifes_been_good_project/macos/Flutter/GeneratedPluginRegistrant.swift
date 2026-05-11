52ef58eb62da53214e6d8a62b0cbbb6f791f83f6
AndroidFreeman <3077128239@qq.com>
2026-05-11 16:02:30 +0800

    Rebuild: lifes_been_good_project/macos/Flutter/GeneratedPluginRegistrant.swift (16:02:30) @Windows

--- lifes_been_good_project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ lifes_been_good_project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,14 @@ import Foundation
 
 import file_picker
 import file_selector_macos
+import sqflite_darwin
+import sqlite3_flutter_libs
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
