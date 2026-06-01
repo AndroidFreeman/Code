037307e8a12dd45a045ed1a26d31d5ee04f273a1
AndroidFreeman <59261712+AndroidFreeman@users.noreply.github.com>
2026-06-01 17:37:31 +0800

    Rebuild: LearnDS_HBPU/0.6_myTree.cpp (17:37:31) @Codespace

--- LearnDS_HBPU/0.6_myTree.cpp
+++ LearnDS_HBPU/0.6_myTree.cpp
@@ -3,7 +3,7 @@
  * @Github: https://github.com/AndroidFreeman
  * Now, I use my Codespace
  * @Author: Android_Freeman
- * @LastEditTime: 2026-05-28 10:34:00
+ * @LastEditTime: 2026-06-01 17:28:10
  * @FilePath: /Code/LearnDS_HBPU/0.6_myTree.cpp
  */
 
@@ -54,9 +54,19 @@ int main() {
     //    4
 
     TreeNode* root = new TreeNode(1);
-    root->left = new TreeNode(2);
-    root->right = new TreeNode(3);
-    root->left->left = new TreeNode(4);
+    // root->left = new TreeNode(2);
+    // root->right = new TreeNode(3);
+    // root->left->left = new TreeNode(4);
+    root->right=new TreeNode(2);
+    root->right->left=new TreeNode(3);
+    root->right->right=new TreeNode(4);
+    root->right->right->left=new TreeNode(5);
+    root->right->right->right=new TreeNode(6);
+    root->right->right->right->left=new TreeNode(7);
+
+
+    
+    
 
     // 测试三种遍历
     std::cout << "前序遍历: ";
